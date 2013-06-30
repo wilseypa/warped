3585f54917140b848242e48598bf1573cee8daf0
XinyuGuo <guox2@mail.uc.edu>
2013-06-30 15:59:36 -0400

    modify functions in DiffusionNetwork class

--- warped/simulationmodels/epidemic/DiffusionNetwork.h
+++ warped/simulationmodels/epidemic/DiffusionNetwork.h
@@ -31,34 +31,27 @@ public:
 	~DiffusionNetwork() {}
 
 	/* To choose or not to choose a random location, that is the question :) */
-	const string &getLocationName() {
+	const string getLocationName() {
 
-		/* Fill this up to return a location of choice */
-		/* It can also choose none, in which case "" will be returned */
-		string nullString = "";
-		string &locationName = nullString;
+		string locationName = "";
 		unsigned int locationNum = travelTimeMap.size();
-		if ( locationNum ) { // there is at least one location in travelTimeMap
-			randNumGen = new RandomNumGen();
-			randNumGen->seedRandNumGen();
+
+		/* Check if there is atleast one location in travelTimeMap */
+		if ( locationNum ) {
 			unsigned int locationID = randNumGen->genRandNum(locationNum);	
+
 			map<string, unsigned int>::iterator mapIter = travelTimeMap.begin();
-			for(int count = 0; count < locationID ; count++ )
+			for(unsigned int count = 0; count < locationID ; count++ ) {
 				mapIter++;
+			}
 			locationName = mapIter->first;
-			return locationName;
 		}
-		else{
-			return locationName;
-		}
+		return locationName;
 	}
 
 	/* Get travel time for the chosen location */
 	unsigned int travelTimeToChosenLoc( string locationChoice ) {
-
-		/* Travel time = travel time from hub to location + travel time from home loc to hub */
-		unsigned int travelTime = travelTimeMap[locationChoice] + travelTimeToHub;
-		return travelTime; 	
+		return (travelTimeMap[locationChoice] + travelTimeToHub);
 	}
 
 	/* Populate the travel cost details */
