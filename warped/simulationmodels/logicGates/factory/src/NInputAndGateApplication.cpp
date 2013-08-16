fdbabcee449261854955010f29f7ea36915b8d28
AJ Alt <aj@alt>
2013-08-16 15:22:50 -0400

    Updated NInputAndGate CLI

--- warped/simulationmodels/logicGates/factory/src/NInputAndGateApplication.cpp
+++ warped/simulationmodels/logicGates/factory/src/NInputAndGateApplication.cpp
@@ -1,24 +1,7 @@
 #include "../include/TwoInputAndGateApplication.h"
 #include "../include/NInputAndGate.h"
-#include "../incoude/"
-
-#include <tclap/CmdLine.h>
 
 int
 TwoInputAndGateApplication::initialize(){
-  try {
-    TCLAP::CmdLine cmd("Two Input AND gate Simulation");
-
-    TCLAP::ValueArg<string> inputFileNameArg("", "simulate", "configuration file name",
-                                              true, inputFileName, "string", cmd);
-
-    cmd.parse(arguments);
-
-    inputFileName = inputFileNameArg.getValue();
-  } catch (TCLAP::ArgException &e) {
-      std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
-      exit(-1);
-  }
-
   return 0;
 }
--- warped/simulationmodels/logicGates/factory/src/NInputAndGateMain.cpp
+++ warped/simulationmodels/logicGates/factory/src/NInputAndGateMain.cpp
@@ -2,9 +2,35 @@
 #include <warped/WarpedMain.h>
 
 int
-main(int argc, char **argv){
-  WarpedMain wm(new TwoInputAndGateApplication());
-  
-  return wm.main(argc, argv);
+main(int argc, char** argv) {
+    std::string configuration = "";
+    std::string simulateUntil = "";
+    bool debug = false;
+
+    try {
+        TCLAP::CmdLine cmd("Two Input And Gate Simulation");
+
+        // Arguments for WarpedMain
+        TCLAP::ValueArg<std::string> configurationArg("c", "configuration", "WARPED configuration file",
+                                                      false, configuration, "file", cmd);
+        TCLAP::ValueArg<std::string> simulateUntilArg("u", "simulateUntil", "specify a simulation end time",
+                                                      false, simulateUntil, "time", cmd);
+        TCLAP::SwitchArg debugArg("d", "debug", "display debug messages", cmd, debug);
+
+        cmd.parse(argc, argv);
+
+        configuration = configurationArg.getValue();
+        simulateUntil = simulateUntilArg.getValue();
+        debug = debugArg.getValue();
+
+    } catch (TCLAP::ArgException& e) {
+        std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+        exit(-1);
+    }
+
+    WarpedMain wm(new TwoInputAndGateApplication(),
+                  configuration, simulateUntil, debug);
+
+    return wm.main(argc, argv);
 }
 
