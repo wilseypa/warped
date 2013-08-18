ee75d00cca09a9aeeefc67c13ff8b2dd9876c849
AJ Alt <aj@alt>
2013-08-18 15:37:32 -0400

    Removed per-file licenses

--- warped/simulationmodels/raid/factory/include/RAIDApplication.h
+++ warped/simulationmodels/raid/factory/include/RAIDApplication.h
@@ -1,24 +1,6 @@
 #ifndef RAID_APPLICATION
 #define RAID_APPLICATION
 
-// Copyright (c) Clifton Labs, Inc.
-// All rights reserved.
-
-// CLIFTON LABS MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
-// SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT
-// NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  CLIFTON LABS SHALL NOT BE
-// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, RESULT
-// OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
-
-// By using or copying this Software, Licensee agrees to abide by the
-// intellectual property laws, and all other applicable laws of the
-// U.S., and the terms of this license.
-
-// Authors:
-//          Dale E. Martin              dmartin@cliftonlabs.com
-//          Randall King                kingr8@mail.uc.edu
-
 #include "warped/Application.h"
 #include "warped/IntVTime.h"
 
