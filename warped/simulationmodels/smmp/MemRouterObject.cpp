651e08398defc923e807ae7fd4c627b9a59176e6
AJ Alt <aj@alt>
2013-08-18 15:38:24 -0400

    Removed per-file licenses

--- warped/simulationmodels/smmp/MemRouterObject.cpp
+++ warped/simulationmodels/smmp/MemRouterObject.cpp
@@ -1,17 +1,3 @@
-// Copyright (c) The University of Cincinnati.  
-// All rights reserved.
-
-// UC MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF 
-// THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-// TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  UC SHALL NOT BE LIABLE
-// FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
-// RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
-// DERIVATIVES.
-// By using or copying this Software, Licensee agrees to abide by the
-// intellectual property laws, and all other applicable laws of the
-// U.S., and the terms of this license.
-
 #include "MemRouterObject.h"
 #include "MemEvents.h"
 #include "MemRouterState.h"
