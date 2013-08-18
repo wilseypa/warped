49c5418dcdc46c31ef12fa8954e18d262b8c01f3
AJ Alt <aj@alt>
2013-08-18 15:38:55 -0400

    Removed per-file licenses

--- warped/simulationmodels/warped/DefaultSchedulingManagerTest.cpp
+++ warped/simulationmodels/warped/DefaultSchedulingManagerTest.cpp
@@ -1,18 +1,3 @@
-// Copyright (c) 2003 Clifton Labs, Inc.  All rights reserved.
-
-// CLIFTON LABS MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
-// SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT
-// NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  CLIFTON LABS SHALL NOT BE
-// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, RESULT
-// OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
-
-// By using or copying this Software, Licensee agrees to abide by the
-// intellectual property laws, and all other applicable laws of the
-// U.S., and the terms of this license.
-
-// Authors: Dale E. Martin              dmartin@cliftonlabs.com
-
 #include "DefaultSchedulingManagerTest.h"
 #include "warped/DefaultSchedulingManager.h"
 
--- warped/simulationmodels/warped/DefaultSchedulingManagerTest.h
+++ warped/simulationmodels/warped/DefaultSchedulingManagerTest.h
@@ -1,21 +1,6 @@
 #ifndef DEFAULT_SCHEDULING_MANAGER_TEST_H
 #define DEFAULT_SCHEDULING_MANAGER_TEST_H
 
-// Copyright (c) 2003 Clifton Labs, Inc.  All rights reserved.
-
-// CLIFTON LABS MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
-// SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT
-// NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  CLIFTON LABS SHALL NOT BE
-// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, RESULT
-// OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
-
-// By using or copying this Software, Licensee agrees to abide by the
-// intellectual property laws, and all other applicable laws of the
-// U.S., and the terms of this license.
-
-// Authors: Dale E. Martin              dmartin@cliftonlabs.com
-
 #include "SchedulingManagerTest.h"
 
 class DefaultSchedulingManager;
