5455afd285682925b22a7c0770354294ab8da82b
AJ Alt <aj@alt>
2013-08-18 15:30:40 -0400

    Removed per-file licenses

--- warped/simulationmodels/pingPong/PingFactoryManager.h
+++ warped/simulationmodels/pingPong/PingFactoryManager.h
@@ -1,28 +1,6 @@
 #ifndef PING_FACTORY_MANAGER_H
 #define PING_FACTORY_MANAGER_H
 
-// Copyright (c) The University of Cincinnati.  
-// All rights reserved.
-
-// UC MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF 
-// THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-// TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  UC SHALL NOT BE LIABLE
-// FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
-// RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
-// DERIVATIVES.
-
-// By using or copying this Software, Licensee agrees to abide by the
-// intellectual property laws, and all other applicable laws of the
-// U.S., and the terms of this license.
-
-// Authors: Malolan Chetlur             mal@ececs.uc.edu
-//          Jorgen Dahl                 dahlj@ececs.uc.edu
-//          Dale E. Martin              dmartin@ececs.uc.edu
-//          Radharamanan Radhakrishnan  ramanan@ececs.uc.edu
-//          Dhananjai Madhava Rao       dmadhava@ececs.uc.edu
-//          Philip A. Wilsey            phil.wilsey@uc.edu
-
 #include "warped.h"
 #include "FactoryManager.h"
 
