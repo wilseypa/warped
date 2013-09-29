28918325d770ae17a8fbf30b1625ebcd94952e43
AJ Alt <aj@alt>
2013-09-29 18:14:17 -0400

    Added more tests

--- test/catch_tests/test_SimulationConfiguration.cpp
+++ test/catch_tests/test_SimulationConfiguration.cpp
@@ -6,18 +6,62 @@
 
 #include "SimulationConfiguration.h"
 
-
 TEST_CASE("File is opened", "[configuration]") {
     REQUIRE_NOTHROW(SimulationConfiguration c("test_file.json"));
 }
 
-TEST_CASE("Return types", "[configuration]") {
+TEST_CASE("string return type", "[configuration]") {
     SimulationConfiguration c("test_file.json");
     REQUIRE( c.as_string({"string_value"}) == "success" );
-    REQUIRE( c.as_int({"int_value"}) == 123 );
-    REQUIRE( c.as_int64({"int64_value"}) == 1152921504606846976ull );
-    REQUIRE( c.as_bool({"bool_true"}) == true );
-    REQUIRE( c.as_bool({"bool_false"}) == false );
+}
+
+TEST_CASE("int return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
     REQUIRE( c.as_int({"int_value"}) == 123 );
 }
 
+TEST_CASE("int64 return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.as_int64({"int64_value"}) == 1152921504606846976ull );
+}
+
+TEST_CASE("true return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.as_bool({"bool_true"}) == true );
+}
+
+TEST_CASE("false return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.as_bool({"bool_false"}) == false );
+}
+
+TEST_CASE("test single nesting", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.as_string({"nested_value", "string_value"}) == "success" );
+}
+
+TEST_CASE("test double nesting", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.as_string({"nested_value", "nested_value", "string_value"}) == "success" );
+}
+
+TEST_CASE("default string return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.get_string({"missing_value"}, "success") == "success" );
+}
+TEST_CASE("default int return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.get_int({"missing_value"}, 123) == 123 );
+}
+TEST_CASE("default int64 return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.get_int64({"missing_value"}, 1152921504606846976ull) == 1152921504606846976ull );
+}
+TEST_CASE("default true return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.get_bool({"missing_value"}, true) == true );
+}
+TEST_CASE("default false return type", "[configuration]") {
+    SimulationConfiguration c("test_file.json");
+    REQUIRE( c.get_bool({"missing_value"}, false) == false );
+}
