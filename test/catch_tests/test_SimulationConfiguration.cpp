763dd4ef92516668bfcdb19a94641a4b0997a50d
AJ Alt <aj@alt>
2013-10-08 13:28:28 -0400

    Refactored and removed external json file

--- test/catch_tests/test_SimulationConfiguration.cpp
+++ test/catch_tests/test_SimulationConfiguration.cpp
@@ -6,62 +6,82 @@
 
 #include "SimulationConfiguration.h"
 
+#include <sstream>
+
 TEST_CASE("File is opened", "[configuration]") {
-    REQUIRE_NOTHROW(SimulationConfiguration c("test_file.json"));
+    std::stringstream ss;
+    ss << "{}";
+    REQUIRE_NOTHROW(SimulationConfiguration c(ss));
 }
 
-TEST_CASE("string return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.as_string({"string_value"}) == "success");
+TEST_CASE("configuration values are read correctly", "[configuration]") {
+    std::stringstream ss;
+    // Use a raw string literal delimited by `"x(` and `)x"` so that all the
+    // quotes in the json don't have to be escaped.
+    ss <<   R"x({
+                    "string_value": "success",
+                    "int_value": 123,
+                    "int64_value": 1152921504606846976, // 2^60
+                    "bool_true": true,
+                    "bool_false": false,
+                    "double_value": 0.75, // 0b0.11
+                    "nested_value": {
+                        "string_value": "success",
+                        "nested_value": {
+                            "string_value": "success"
+                        }
+                    }
+                })x";
+
+    SimulationConfiguration c(ss);
+
+    SECTION("string return type") {
+        REQUIRE(c.as_string({"string_value"}) == "success");
+    }
+
+    SECTION("int return type") {
+        REQUIRE(c.as_int({"int_value"}) == 123);
+    }
+
+    SECTION("int64 return type") {
+        REQUIRE(c.as_int64({"int64_value"}) == 1152921504606846976ull);
+    }
+
+    SECTION("true return type") {
+        REQUIRE(c.as_bool({"bool_true"}) == true);
+    }
+
+    SECTION("false return type") {
+        REQUIRE(c.as_bool({"bool_false"}) == false);
+    }
+
+    SECTION("test single nesting") {
+        REQUIRE(c.as_string({"nested_value", "string_value"}) == "success");
+    }
+
+    SECTION("test double nesting") {
+        REQUIRE(c.as_string({"nested_value", "nested_value", "string_value"}) == "success");
+    }
+
+    SECTION("default string return type") {
+        REQUIRE(c.get_string({"missing_value"}, "success") == "success");
+    }
+
+    SECTION("default int return type") {
+        REQUIRE(c.get_int({"missing_value"}, 123) == 123);
+    }
+
+    SECTION("default int64 return type") {
+        REQUIRE(c.get_int64({"missing_value"}, 1152921504606846976ull) == 1152921504606846976ull);
+    }
+
+    SECTION("default true return type") {
+        REQUIRE(c.get_bool({"missing_value"}, true) == true);
+    }
+
+    SECTION("default false return type") {
+        REQUIRE(c.get_bool({"missing_value"}, false) == false);
+    }
 }
 
-TEST_CASE("int return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.as_int({"int_value"}) == 123);
-}
 
-TEST_CASE("int64 return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.as_int64({"int64_value"}) == 1152921504606846976ull);
-}
-
-TEST_CASE("true return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.as_bool({"bool_true"}) == true);
-}
-
-TEST_CASE("false return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.as_bool({"bool_false"}) == false);
-}
-
-TEST_CASE("test single nesting", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.as_string({"nested_value", "string_value"}) == "success");
-}
-
-TEST_CASE("test double nesting", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.as_string({"nested_value", "nested_value", "string_value"}) == "success");
-}
-
-TEST_CASE("default string return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.get_string({"missing_value"}, "success") == "success");
-}
-TEST_CASE("default int return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.get_int({"missing_value"}, 123) == 123);
-}
-TEST_CASE("default int64 return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.get_int64({"missing_value"}, 1152921504606846976ull) == 1152921504606846976ull);
-}
-TEST_CASE("default true return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.get_bool({"missing_value"}, true) == true);
-}
-TEST_CASE("default false return type", "[configuration]") {
-    SimulationConfiguration c("test_file.json");
-    REQUIRE(c.get_bool({"missing_value"}, false) == false);
-}
