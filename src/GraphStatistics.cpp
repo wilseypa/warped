b68602d77357f43e8817c9f540530e2cb9dfc927
AJ Alt <aj@alt>
2014-02-08 14:02:34 -0500

    Switched to undirected graph representation

--- src/GraphStatistics.cpp
+++ src/GraphStatistics.cpp
@@ -1,8 +1,24 @@
-#include <string>
-#include <iostream>
-#include <fstream>
-
 #include "GraphStatistics.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+
+namespace {
+Edge make_edge(int a, int b) {
+    return (a <= b) ? std::make_pair(a, b) : std::make_pair(b, a);
+}
+} // namespace
+
+void GraphStatistics::update_edge_stat(int a, int b, const std::string& stat, int x)  {
+    edge_stats[make_edge(a, b)][stat] += x;
+}
+
+int GraphStatistics::get_edge_stat(int a, int b, const std::string& stat) {
+    return edge_stats[make_edge(a, b)][stat];
+}
+
 
 void GraphStatistics::output_dot_file(const std::string& filename) {
     std::ofstream file;
@@ -14,15 +30,15 @@ void GraphStatistics::output_dot_file(const std::string& filename) {
         // It would probably be a good idea to add the stat to the label if
         // there's more than one.
         for (auto stats_it : edges_it.second) {
-            if(first_stat) {
+            if (first_stat) {
                 file << " [label=\"";
-                    first_stat = false;
+                first_stat = false;
             } else {
                 file << ", ";
             }
             file << stats_it.second;
         }
-        if(!first_stat) {
+        if (!first_stat) {
             file << "\"]";
         }
         file << ";\n";
--- src/GraphStatistics.h
+++ src/GraphStatistics.h
@@ -1,19 +1,14 @@
 #include <iostream>
 #include <map>
-#include <utility>
 #include <string>
+#include <utility>
 
 typedef std::pair<int, int> Edge;
 
 class GraphStatistics {
 public:
-    inline void update_edge_stat(int a, int b, const std::string& stat, int x = 1)  {
-        edge_stats[std::make_pair(a, b)][stat] += x;
-    }
-
-    inline int get_edge_stat(int a, int b, const std::string& stat) {
-        return edge_stats[std::make_pair(a, b)][stat];
-    }
+    void update_edge_stat(int a, int b, const std::string& stat, int x = 1);
+    int get_edge_stat(int a, int b, const std::string& stat); 
 
     void output_dot_file(const std::string& filename);
 
