90f05cce1784ee6c311d431c7e4f6be12f7c04fe
AJ Alt <aj@alt>
2014-02-14 17:19:21 -0500

    Add metis output format

--- src/GraphStatistics.cpp
+++ src/GraphStatistics.cpp
@@ -1,9 +1,7 @@
 #include "GraphStatistics.h"
 
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <utility>
+#include <algorithm>
+#include <set>
 
 namespace {
 Edge make_edge(int a, int b) {
@@ -21,20 +19,27 @@ int GraphStatistics::get_edge_stat(int a, int b, const std::string& stat) {
 
 void GraphStatistics::write_to_file(const std::string& format,
                                     const std::string& filename) {
-    if (format == "dot") {
-        output_dot_file(filename);
+    std::ofstream file;
+    if (format == "graphviz" || format == "metis") {
+        file.open(filename);
+        if (!file.is_open()) {
+            std::cerr << "Could not open file " << filename << std::endl;
+            return;
+        }
+    }
+
+    if (format == "graphviz") {
+        output_graphviz_file(file);
+    } else if (format == "metis") {
+        output_metis_file(file);
     } else {
         std::cerr << "Unsupported statistics filetype: " << filename << std::endl;
-    }
-}
-
-void GraphStatistics::output_dot_file(const std::string& filename) {
-    std::ofstream file;
-    file.open(filename);
-    if (!file.is_open()) {
-        std::cerr << "Could not open file " << filename << std::endl;
         return;
     }
+    file.close();
+}
+
+void GraphStatistics::output_graphviz_file(std::ofstream& file) {
     file << "graph {\n";
     for (auto edges_it : edge_stats) {
         file << edges_it.first.first << " -- " << edges_it.first.second;
@@ -56,6 +61,62 @@ void GraphStatistics::output_dot_file(const std::string& filename) {
         file << ";\n";
 
     }
-    file << "}";
-    file.close();
+    file << "}\n";
+}
+
+void GraphStatistics::output_metis_file(std::ofstream& file) {
+    // Metis files require a list of every node, so we have to build a map of
+    // edges in both directions.
+    std::map<Edge, std::map<std::string, int>> full_graph(edge_stats);
+
+    // Count the vertices and stats.
+    std::set<int> vertices;
+    std::set<std::string> stats;
+    for (auto edges_it : edge_stats) {
+        vertices.insert(edges_it.first.first);
+        vertices.insert(edges_it.first.second);
+        for (auto stats_it : edges_it.second) {
+            full_graph[std::make_pair(edges_it.first.second,
+                                      edges_it.first.first)][stats_it.first] = stats_it.second;
+            stats.insert(stats_it.first);
+        }
+    }
+    int numEdges = edge_stats.size();
+    int numVertices = vertices.size();
+    int numWeights = stats.size();
+
+    file << "%% The first line contains the following information:\n"
+         << "%% <# of vertices> <# of edges> <file format> <# of weights>\n"
+         <<  numVertices << ' ' << numEdges << ' ' << "001 " << numWeights << '\n'
+         << "%% The remaining lines each describe a vertex and have the following format:\n"
+         << "%% <<neighbor>";
+    for (auto& stat : stats) {
+        file << " <" << stat << '>';
+    }
+    file << "> ...\n";
+
+    // Metis requires node numbering start at 1 instead of 0, so add one to
+    // all indices if node 0 exists
+    int offset = vertices.count(0);
+    // Keep track of the last vertex, since each vertex is on its own line
+    int last_vertex = -1;
+    for (auto edges_it : full_graph) {
+        // Metis doesn't accept self-edges
+        if (edges_it.first.first == edges_it.first.second) {
+            continue;
+        }
+        if (edges_it.first.first != last_vertex) {
+            if (last_vertex >= 0) { file << '\n'; }
+            last_vertex = edges_it.first.first;
+        }
+        file << edges_it.first.second + offset << ' ';
+        // iterate over stats instead of edges_it.second in case an edge
+        // doesn't have every weight
+        for (auto stat : stats) {
+            // Metis requires weights be non-zero
+            int weight = std::max(1, edges_it.second[stat]);
+            file << weight << ' ';
+        }
+    }
+    file << '\n';
 }
--- src/GraphStatistics.h
+++ src/GraphStatistics.h
@@ -2,6 +2,7 @@
 #include <map>
 #include <string>
 #include <utility>
+#include <fstream>
 
 typedef std::pair<int, int> Edge;
 
@@ -13,13 +14,17 @@ public:
     /**
     Write the statistics to a file
 
-    @param format The format of the file. Supported formats are "dot"
+    @param format The format of the file. Supported formats are: "graphviz", "metis"
     @param filename The path to the file that will be created.
     */
     void write_to_file(const std::string& format, const std::string& filename);
 
+    /** edge_stats is an ordered map, so iterating over it will group edges
+        together based on their first vertex
+    */
     std::map<Edge, std::map<std::string, int>> edge_stats;
 
 private:
-    void output_dot_file(const std::string& filename);
+    void output_graphviz_file(std::ofstream& file);
+    void output_metis_file(std::ofstream& file);
 };
