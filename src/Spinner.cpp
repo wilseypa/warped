9c567be34d379fb7c44232528a5bd753c68630c7
AJ Alt <aj@alt>
2014-02-27 18:53:14 -0500

    Fix spacing

--- src/Spinner.cpp
+++ src/Spinner.cpp
@@ -12,7 +12,7 @@ void Spinner::spinForever() {
     std::cerr << "The configuration is set to enter an infinite loop. "
               << "You'll need to break it with the "
               << "debugger.  See Spinner: " << __LINE__
-              << "in gdb to break this loop.";
+              << " in gdb to break this loop.";
 #ifdef HAVE_UNISTD_H
     std::cerr << "\npid = " << getpid();
 #endif
