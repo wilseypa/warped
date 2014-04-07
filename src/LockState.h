751afa20c0a303044b463c7d6174c8f541c3ce2d
Josh Hay <hayja@mail.uc.edu>
2014-04-07 14:51:47 -0400

    added padded lock struct to put lock variable on single cache line; added rtm retry backoff; added memory alignment

--- src/LockState.h
+++ src/LockState.h
@@ -11,8 +11,10 @@ static const int NOONE = -1;
 class LockState {
 public:
     LockState() {
-        lockOwner = NOONE;
+        paddedLockOwner.lockOwner = NOONE;
         pthread_mutex_init(&mutexLock, NULL);
+
+        tsxRtmRetries = TSXRTM_RETRIES;
 
         //rtm stats
         tsxCommits = 0;
@@ -26,22 +28,24 @@ public:
     }
     bool releaseLock(const unsigned int& threadNumber, const std::string syncMechanism) {
 #if USETSX_RTM
-        if (lockOwner == NOONE) {
+        if (paddedLockOwner.lockOwner == NOONE) {
             _xend();
             tsxCommits++;
             return true;
         }
 #endif
         if (syncMechanism == "HleAtomicLock") {
-            while (!_xrelease(&lockOwner, &threadNumber));
+            if (_xtest()) { tsxCommits++; }
+            else { tsxAborts++; }
+            while (!_xrelease(&paddedLockOwner.lockOwner, &threadNumber));
             return true;
         } else if (syncMechanism == "AtomicLock") {
             //If Currently Working and we can set it to Available then return true else return false;
-            return __sync_bool_compare_and_swap(&lockOwner, threadNumber, NOONE);
+            return __sync_bool_compare_and_swap(&paddedLockOwner.lockOwner, threadNumber, NOONE);
         } else if (syncMechanism == "Mutex") {
             if (!pthread_mutex_lock(&mutexLock)) {
-                if (threadNumber == (unsigned int) lockOwner) {
-                    lockOwner = NOONE;
+                if (threadNumber == (unsigned int) paddedLockOwner.lockOwner) {
+                    paddedLockOwner.lockOwner = NOONE;
                     pthread_mutex_unlock(&mutexLock);
                     return true;
                 } else {
@@ -63,15 +67,16 @@ public:
         int retries = 0;
 
         if (tsxCommits == 0 && tsxAborts > TSXRTM_RETRIES * 100) { }
-        else { 
-            while (retries++ < TSXRTM_RETRIES) {
+        else if (tsxRtmRetries > 1 && tsxAborts > tsxCommits << 1) {
+            tsxRtmRetries--;
+        } else {
+            do {
                 status = _xbegin();
                 if (status == _XBEGIN_STARTED) {
-                    if (lockOwner == NOONE) {
+                    if (paddedLockOwner.lockOwner == NOONE) {
                        return;
                     }
                     _xabort(_ABORT_LOCK_BUSY);
-                    break;
                 }
                 ABORT_COUNT(_XA_RETRY, status);
                 ABORT_COUNT(_XA_EXPLICIT, status);
@@ -84,27 +89,28 @@ public:
                 } else if ((status & _XABORT_EXPLICIT) && _XABORT_CODE(status) == _ABORT_LOCK_BUSY) {
                     while (isLocked());
                 } else if (status & _XABORT_CONFLICT) {
-                    delay();
+                    _mm_pause();
                 }
-            }
+            } while (retries++ < tsxRtmRetries);
             tsxAborts++;
         }
 #endif
         if (syncMechanism == "HleAtomicLock") {
-            while(!_xacquire(&lockOwner, &threadNumber));
+            if( paddedLockOwner.lockOwner == threadNumber ) return;
+            while(!_xacquire(&paddedLockOwner.lockOwner, &threadNumber));
         } else if (syncMechanism == "AtomicLock") {
             //If Available and we can set it to Working then return true else return false;
-            if( lockOwner == threadNumber ) return;
-            while (!__sync_bool_compare_and_swap(&lockOwner, NOONE, threadNumber));
+            if( paddedLockOwner.lockOwner == threadNumber ) return;
+            while (!__sync_bool_compare_and_swap(&paddedLockOwner.lockOwner, NOONE, threadNumber));
         } else if (syncMechanism == "Mutex") {
             bool locked = false;
             while (!locked) {
                 if (!pthread_mutex_lock(&mutexLock)) {
-                    if (lockOwner == NOONE) {
-                        lockOwner = threadNumber;
+                    if (paddedLockOwner.lockOwner == NOONE) {
+                        paddedLockOwner.lockOwner = threadNumber;
                         pthread_mutex_unlock(&mutexLock);
                         locked = true;
-                    } else if(lockOwner == (int) threadNumber) {
+                    } else if(paddedLockOwner.lockOwner == (int) threadNumber) {
                         pthread_mutex_unlock(&mutexLock);
                         locked = true;
                     } else {
@@ -122,27 +128,27 @@ public:
     }
     bool hasLock(const unsigned int& threadNumber, const std::string syncMechanism) const {
         if (syncMechanism == "HleAtomicLock") {
-            return (threadNumber == (unsigned int) lockOwner);
+            return (threadNumber == (unsigned int) paddedLockOwner.lockOwner);
         } else if (syncMechanism == "AtomicLock") {
-            return (threadNumber == (unsigned int) lockOwner);
+            return (threadNumber == (unsigned int) paddedLockOwner.lockOwner);
         } else if (syncMechanism == "Mutex") {
-            return (threadNumber == (unsigned int) lockOwner);
+            return (threadNumber == (unsigned int) paddedLockOwner.lockOwner);
         } else {
             std::cout << "Invalid sync mechanism" << std::endl;
             return false;
         }
     }
     void showStatus() {
-        std::cout << "Locked By: " << lockOwner << std::endl;
+        std::cout << "Locked By: " << paddedLockOwner.lockOwner << std::endl;
     }
     bool isLocked() {
-        if (this->lockOwner == NOONE)
+        if (this->paddedLockOwner.lockOwner == NOONE)
         { return false; }
         else
         { return true; }
     }
     int whoHasLock() {
-        return lockOwner;
+        return paddedLockOwner.lockOwner;
     }
     void reportTSXstats() {
         std::cout << "Total commits: " << tsxCommits << std::endl;
@@ -153,13 +159,19 @@ public:
         std::cout << "\t_XA_CAPACITY: " << tsxAbrtType[_XA_CAPACITY] << std::endl;
     }
 private:
-    int lockOwner;
+    struct {
+        int lockOwner;
+        char padding[60];
+    } paddedLockOwner __attribute__((aligned(L1DSZ)));
     pthread_mutex_t mutexLock;
+
+    //rtm retries
+    int tsxRtmRetries;
 
     //rtm stats
     long tsxCommits;
     long tsxAborts;
     unsigned tsxAbrtType[4];
-};
+} __attribute__((aligned(L1DSZ)));
 
 #endif /* LockState_H_ */
